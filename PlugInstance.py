c38a383fcf154ff0398ab7bbcda524b3c1e1ff97
kbickar <trumpetgod@gmail.com>
2020-08-20 16:06:08 -0400

    Stripped out non-essential code

--- PlugInstance.py
+++ PlugInstance.py
@@ -1,110 +1,45 @@
 # Copyright 2020, Charles Powell
 
-import random
-import logging
-from DataSource import DataSource
-from typing import Type
+import hashlib
 from time import time
 
-
-# Generator for random MAC address
-# Thanks to @pklaus: https://gist.github.com/pklaus/9638536
-def random_bytes(num=6):
-    return [random.randrange(256) for _ in range(num)]
-
-
-def generate_mac(uaa=False, multicast=False, oui=None, separator=':', byte_fmt='%02x'):
-    mac = random_bytes()
-    if oui:
-        if type(oui) == str:
-            oui = [int(chunk, 16) for chunk in oui.split(separator)]
-        mac = oui + random_bytes(num=6 - len(oui))
-    else:
-        if multicast:
-            mac[0] |= 1  # set bit 0
-        else:
-            mac[0] &= ~1  # clear bit 0
-        if uaa:
-            mac[0] &= ~(1 << 1)  # clear bit 1
-        else:
-            mac[0] |= 1 << 1  # set bit 1
-    return separator.join(byte_fmt % b for b in mac)
-
-
-def generate_deviceid():
-    deviceid_bytes = random_bytes(num=20)
-    return ''.join('%02x' % b for b in deviceid_bytes)
-
-
 class PlugInstance:
-    start_time = None
-    data_source = None
 
-    def __init__(self, identifier, alias=None, mac=None, device_id=None):
-        self.identifier = identifier
-        if mac is None:
-            gen_mac = generate_mac(oui='53:75:31')
-            logging.info("Spoofed MAC: %s", gen_mac)
-            self.mac = gen_mac
-        else:
-            self.mac = mac
+    def __init__(self, alias, start_time, power=0, current = 0, voltage=120, mac=None, device_id=None):      
+        self.alias = alias
+        self.start_time = start_time
+        
+        self.voltage = voltage
+        self.power = power
+        self.current = current
+        
+        if not self.power:
+            self.power = self.voltage*self.current
+        if not self.current:
+            self.current = self.power/self.voltage
 
-        if device_id is None:
-            gen_device_id = generate_deviceid()
-            logging.info("Spoofed Device ID: %s", gen_device_id)
-            self.device_id = gen_device_id
-        else:
-            self.device_id = device_id
+        if device_id: self.device_id = device_id
+        else: self.device_id = self.generate_deviceid()
+        
+        if mac: self.mac = mac
+        else: self.mac = self.generate_mac()
 
-        if alias is None:
-            self.alias = "Spoofed TP-Link Kasa HS110 " + self.device_id[0:8]
-        else:
-            self.alias = alias
+    def generate_mac(self):
+        end = [int(self.device_id[i:i+2],16) for i in range(0, len(self.device_id), 2)]
+        mac = [53, 75, 31] + end[:3]
+        return ':'.join('%02x' % b for b in mac)
 
-    @classmethod
-    # Convenience method to create a lot of plugs
-    def configure_plugs(cls, plugs, data_source_class: Type[DataSource], data_controller=None):
-        # Loop through all plugs
-        instances = []
-        for plug in plugs:
-            # Get specified identifier
-            plug_id = next(iter(plug.keys()))
-            # Get plug details
-            details = plug.get(plug_id)
-            if details is not None:
-                # Define main details
-                alias = details.get('alias')
-                mac = details.get('mac')
-                device_id = details.get('device_id')
-
-                # Create and configure instance
-                instance = cls(plug_id, alias, mac, device_id)
-
-                # Generate data source with details, and assign
-                instance.data_source = data_source_class(plug_id, details, data_controller)
-                instances.append(instance)
-
-                logging.debug(f"Added plug: {plug_id}")
-
-        return instances
-
-    @property
-    def power(self):
-        return self.data_source.get_power()
+    def generate_deviceid(self):
+        return hashlib.sha1(self.alias.encode('utf-8')).hexdigest()
 
     def generate_response(self):
-        # Grab latest values from source
-        power = self.data_source.get_power()
-        current = self.data_source.get_current()
-        voltage = self.data_source.voltage
-
         # Response dict
         response = {
             "emeter": {
                 "get_realtime": {
-                    "current": current,
-                    "voltage": voltage,
-                    "power": power,
+                    "current": self.current,
+                    "voltage": self.voltage,
+                    "power": self.power,
                     "total": 0,     # Unsure if this needs a value, appears not to
                     "err_code": 0   # No errors here!
                 }
@@ -137,11 +72,4 @@ class PlugInstance:
             }
         }
         return response
-
-
-if __name__ == "__main__":
-    # Convenience function to generate a MAC address and Device ID
-    gen_device_id = generate_deviceid()
-    print(f"Generated Device ID:   {gen_device_id.upper()}")
-    gen_mac = generate_mac(oui='50:c7:bf')
-    print(f"Generated MAC:         {gen_mac.upper()}")
+ 
--- SenseLink.py
+++ SenseLink.py
@@ -1,181 +1,96 @@
 # Copyright 2020, Charles Powell
-
+import logging
 import asyncio
 import json
-import time
-import yaml
-import argparse
-from DataSource import *
-from PlugInstance import *
-from TPLinkEncryption import *
-from aioudp import *
-import nest_asyncio
-nest_asyncio.apply()
+import TPLinkEncryption
+from PlugInstance import PlugInstance
 
+SENSE_TP_LINK_PORT = 9999
 
-# Check if a multi-layer key exists
-def keys_exist(element, *keys):
-    if not isinstance(element, dict):
-        raise AttributeError('keys_exists() expects dict as first argument.')
-    if len(keys) == 0:
-        raise AttributeError('keys_exists() expects at least two arguments, one given.')
+class SenseLinkServerProtocol:
+    def __init__(self, devices):
+        self._devices = devices
+        self.should_respond = True
+        
+    def connection_made(self, transport):
+        self.transport = transport
+        
+    def connection_lost(self, exc):
+        pass
 
-    _element = element
-    for key in keys:
+    def datagram_received(self, data, addr):
+        decrypted_data = TPLinkEncryption.decrypt(data)
+
         try:
-            _element = _element[key]
-        except KeyError:
-            return False
-    return True
+            json_data = json.loads(decrypted_data)
+            # Sense requests the emeter and system parameters
+            if 'emeter' in json_data and 'get_realtime' in json_data["emeter"] and \
+               'system' in json_data and 'get_sysinfo' in json_data["system"]:
+                # Check for non-empty values, to prevent echo storms
+                if json_data['emeter']['get_realtime']:
+                    # This is a self-echo, common with Docker without --net=Host!
+                    logging.debug("Ignoring non-empty/non-Sense UDP request")
+                    return
+
+                logging.debug("Broadcast received from: %s: %s", addr, json_data)
+
+                # Build and send responses
+                for plug in self._devices():
+                    # Build response
+                    response = plug.generate_response()
+                    json_resp = json.dumps(response, separators=(',', ':'))
+                    encrypted_resp = TPLinkEncryption.encrypt(json_resp)
+                    # Strip leading 4 bytes for...some reason
+                    encrypted_resp = encrypted_resp[4:]
+
+                    # Allow disabling response
+                    if self.should_respond:
+                        # Send response
+                        logging.debug(f"Sending response: {response}")
+                        self.transport.sendto(encrypted_resp, addr)
+                    else:
+                        # Do not send response, but log for debugging
+                        logging.debug(f"SENSE_RESPONSE disabled, response content: {response}")
+            else:
+                logging.info(f"Unexpected/unhandled message: {json_data}")
+
+        # Appears to not be JSON
+        except ValueError:
+            logging.debug("Did not receive valid json")
 
 
 class SenseLink:
-    _remote_ep = None
-    _local_ep = None
-    _instances = []
-    should_respond = True
+    _devices = []
 
-    def __init__(self, config, port=9999):
-        self.config = config
+    def __init__(self, devices, port=SENSE_TP_LINK_PORT):
         self.port = port
-        self.server_task = None
-
-    def create_instances(self):
-        config = yaml.load(self.config, Loader=yaml.FullLoader)
-        logging.debug(f"Configuration loaded: {config}")
-        sources = config.get('sources')
-        for source in sources:
-            # Get specified identifier
-            source_id = next(iter(source.keys()))
-            logging.debug(f"Adding {source_id} configuration")
-            # Static value plugs
-            if source_id.lower() == "static":
-                # Static sources require no extra config
-                static = source['static']
-                if static is None:
-                    logging.error(f"Configuration error for Source {source_id}")
-                # Generate plug instances
-                plugs = static['plugs']
-                instances = PlugInstance.configure_plugs(plugs, DataSource)
-                self._instances.extend(instances)
-
-            # HomeAssistant Plugs, using Websockets datasource
-            elif source_id.lower() == "hass":
-                # Configure this HASS Data source
-                hass = source['hass']
-                if hass is None:
-                    logging.error(f"Configuration error for Source {source_id}")
-                url = hass['url']
-                auth_token = hass['auth_token']
-                print(f"{url}, {auth_token}")
-                ds_controller = HASSController(url, auth_token)
-
-                # Generate plug instances
-                plugs = hass['plugs']
-                print("Generating instances")
-                instances = PlugInstance.configure_plugs(plugs, HASSSource, ds_controller)
-
-                # Add instances to self
-                self._instances.extend(instances)
-
-                # Start controller
-                ds_controller.connect()
-
-            else:
-                logging.error(f"Source type {source_id} not recognized")
+        self._devices = devices
 
     def print_instance_wattages(self):
-        for inst in self._instances:
-            logging.info(f"Plug {inst.identifier} power: {inst.power}")
+        for inst in self._devices():
+            logging.info(f"Plug {inst.alias} power: {inst.power}")
 
     async def start(self):
-        self.create_instances()
-        await self._serve()
+        loop = asyncio.get_running_loop()
+        self.transport, self.protocol = await loop.create_datagram_endpoint(
+            lambda: SenseLinkServerProtocol(self._devices),
+            local_addr=('0.0.0.0', self.port))
+            
+    async def stop(self):
+        self.transport.close()
 
-    async def _serve(self):
-        server_start = time()
-        logging.info("Starting UDP server")
-        self._local_ep = await open_local_endpoint('0.0.0.0', self.port)
-
-        while True:
-            data, addr = await self._local_ep.receive()
-            request_addr = addr[0]
-            decrypted_data = decrypt(data)
-
-            try:
-                json_data = json.loads(decrypted_data)
-                # Sense requests the emeter and system parameters
-                if keys_exist(json_data, "emeter", "get_realtime") and keys_exist(json_data, "system", "get_sysinfo"):
-                    # Check for non-empty values, to prevent echo storms
-                    if bool(safekey(json_data, 'emeter/get_realtime')):
-                        # This is a self-echo, common with Docker without --net=Host!
-                        logging.debug("Ignoring non-empty/non-Sense UDP request")
-                        continue
-
-                    logging.debug("Broadcast received from: %s: %s", request_addr, json_data)
-
-                    if self._remote_ep is None:
-                        self._remote_ep = await open_remote_endpoint(request_addr, self.port)
-
-                    # Build and send responses
-                    for inst in self._instances:
-                        if inst.start_time is None:
-                            inst.start_time = server_start
-                        # Build response
-                        response = inst.generate_response()
-                        json_str = json.dumps(response, separators=(',', ':'))
-                        encrypted_str = encrypt(json_str)
-                        # Strip leading 4 bytes for...some reason
-                        trun_str = encrypted_str[4:]
-
-                        # Allow disabling response
-                        if self.should_respond:
-                            # Send response
-                            logging.debug(f"Sending response: {response}")
-                            self._remote_ep.send(trun_str)
-                        else:
-                            # Do not send response, but log for debugging
-                            logging.debug(f"SENSE_RESPONSE disabled, response content: {response}")
-                else:
-                    logging.info(f"Unexpected/unhandled message: {json_data}")
-
-            # Appears to not be JSON
-            except ValueError:
-                logging.debug("Did not receive valid json")
-                return True
-
-
-async def main():
-    import os
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="specify config file path")
-    parser.add_argument("-l", "--log", help="specify log level (DEBUG, INFO, etc)")
-    parser.add_argument("-q", "--quiet", help="do not respond to Sense UPD queries", action="store_true")
-    args = parser.parse_args()
-    config_path = args.config or '/etc/senselink/config.yml'
-    loglevel = args.log or 'WARNING'
-
-    loglevel = os.environ.get('LOGLEVEL', loglevel).upper()
-    logging.basicConfig(level=loglevel)
-
-    # Assume config file is in etc directory
-    config_location = os.environ.get('CONFIG_LOCATION', config_path)
-    logging.debug(f"Using config at: {config_location}")
-    config = open(config_location, 'r')
-
-    # Create controller, with config
-    controller = SenseLink(config)
-    if os.environ.get('SENSE_RESPONSE', 'True').upper() == 'TRUE' and not args.quiet:
-        logging.info("Will respond to Sense broadcasts")
-        controller.should_respond = True
-
-    # Start and run indefinitely
-    logging.info("Starting SenseLink controller")
-    loop = asyncio.get_event_loop()
-    loop.create_task(controller.start())
-    loop.run_forever()
-
+async def test():
+    import time
+    def test_devices():
+        devices = [PlugInstance("Lamp", time()-20, power=10), PlugInstance("Fan", time()-20, power=14)]
+        for d in devices:
+            yield d
+    sl = SenseLink(test_devices)
+    await sl.start()
+    try:
+        await asyncio.sleep(180)  # Serve for 3 minutes
+    finally:
+        await sl.stop()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(test())
